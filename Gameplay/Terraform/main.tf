e1928240a51115e3ffe37b35b1ab496805d72265
meghaabhardwajj <159884930+meghaabhardwajj@users.noreply.github.com>
2025-09-11 19:53:34 +0530

    Update main.tf

--- Gameplay/Terraform/main.tf
+++ Gameplay/Terraform/main.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_service" "concept_storyline" {
   template {
     spec {
       containers {
-        image = "us-east1-docker.pkg.dev/blkbox-infrastructure/blkbox-main/blkbox-gameplay/concept-storyline-service:${var.concept_storyline_image_tag}"
+        image = "us-east1-docker.pkg.dev/blkbox-infrastructure/blkbox-main/blkbox-gameplay/concept-storyline-service:${var.dev_concept_storyline_image_tag}"
       }
     }
   }
