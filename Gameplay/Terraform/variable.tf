0ec7b50812af78f1b8b67f12e8088e4c1586e95a
meghaabhardwajj <159884930+meghaabhardwajj@users.noreply.github.com>
2025-09-11 19:08:07 +0530

    Create variable.tf

---
+++ Gameplay/Terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "concept_storyline_image_tag" {
+default = "dev_latest"
+}
+
+variable "messaging_image_tag" {
+default = "dev_latest"
+}
+
+variable "scene_prediction_image_tag" {
+default = "dev_latest"
+}
+
+variable "user_persona_image_tag" {
+default = "dev_latest"
+}
+
