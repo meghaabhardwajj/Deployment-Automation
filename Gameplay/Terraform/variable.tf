991e76b7f1ece49690a909cd11586f9178e1aee7
meghaabhardwajj <159884930+meghaabhardwajj@users.noreply.github.com>
2025-09-11 19:50:45 +0530

    Update variable.tf

--- Gameplay/Terraform/variable.tf
+++ Gameplay/Terraform/variable.tf
@@ -1,16 +1,11 @@
-variable "concept_storyline_image_tag" {
-default = "dev_latest"
-}
+variable "dev_concept_storyline_image_tag" {}
+variable "prod_concept_storyline_image_tag" {}
 
-variable "messaging_image_tag" {
-default = "dev_latest"
-}
+variable "dev_messaging_image_tag" {}
+variable "prod_messaging_image_tag" {}
 
-variable "scene_prediction_image_tag" {
-default = "dev_latest"
-}
+variable "dev_scene_prediction_image_tag" {}
+variable "prod_scene_prediction_image_tag" {}
 
-variable "user_persona_image_tag" {
-default = "dev_latest"
-}
-
+variable "dev_user_persona_image_tag" {}
+variable "prod_user_persona_image_tag" {}
