cde48a94f7cd228c4b6718c0a92c3f00870b4502
meghaabhardwajj <159884930+meghaabhardwajj@users.noreply.github.com>
2025-09-11 19:56:53 +0530

    Update main.tf

--- Gameplay/Terraform/main.tf
+++ Gameplay/Terraform/main.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_run_service" "concept_storyline" {
+resource "google_cloud_run_service" "concept_storyline_dev" {
   name     = "dev-concept-storyline-service"
   location = "us-east1"
 
@@ -11,3 +11,43 @@ resource "google_cloud_run_service" "concept_storyline" {
   }
 }
 
+resource "google_cloud_run_service" "concept_storyline_prod" {
+  name     = "prod-concept-storyline-service"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "us-east1-docker.pkg.dev/blkbox-infrastructure/blkbox-main/blkbox-gameplay/concept-storyline-service:${var.prod_concept_storyline_image_tag}"
+      }
+    }
+  }
+}
+
+
+resource "google_cloud_run_service" "messaging_service_dev" {
+  name     = "dev-messaging-service"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "us-east1-docker.pkg.dev/blkbox-infrastructure/blkbox-main/blkbox-gameplay/concept-storyline-service:${var.dev_messaging_image_tag}"
+      }
+    }
+  }
+}
+
+
+resource "google_cloud_run_service" "messaging_service_prod" {
+  name     = "prod-messaging-service"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "us-east1-docker.pkg.dev/blkbox-infrastructure/blkbox-main/blkbox-gameplay/concept-storyline-service:${var.prod_messaging_image_tag}"
+      }
+    }
+  }
+}
